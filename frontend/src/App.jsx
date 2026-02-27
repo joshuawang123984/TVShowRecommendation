20591b5f53e38623f2241b9d109fc1ce26240956
Joshua Wang <joshuawang9999@gmail.com>
2026-02-26 23:34:18 -0500

    added components to webpage, now need styling. also need to properly set up on render.com

---
+++ frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+
+export default function App() {
+  const [input, setInput] = useState("")
+  const { results, loading, error, searched, fetchRecommendations } = useFetch();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetchRecommendations(input);
+  }
+
+  return (<>
+    <div className="headr">
+      <h1 className="">TV Show Recommendation</h1>
+      <p className="">Enter a show name to find 5 similar shows</p>
+    </div>
+
+    <form onSubmit={handleSubmit} className="">
+      <input
+        className=""
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="e.g. The Walking Dead"
+        autoFocus
+      />
+      <button
+        type="submit"
+        className=""
+      >
+        search
+      </button>
+    </form>
+
+    {
+      loading && (
+        <p className="">searching...</p>
+      )
+    }
+
+    {
+      error && (
+        <p className="">error: {error}</p>
+      )
+    }
+
+    {
+      results.length > 0 && (
+        <div className="flex flex-col gap-3">
+          <p className="">recommendations for '{searched}':</p>
+          {results.map((r, i) => (
+            <div key={i} className="">
+              <span className="text-white">{r.title}</span>
+              <span className="">similarity: {r.score}</span>
+            </div>
+          ))}
+        </div>
+      )
+    }
+  </>
+  )
+}
